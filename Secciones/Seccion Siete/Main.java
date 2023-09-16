b472ed1b9109f91e5af21dbf57277f08f49b637c
Daniel Franco <dfrancohenao@gmail.com>
2023-09-15 21:48:43 -0500

    Adding constructors syntax

--- Secciones/Seccion Siete/Main.java
+++ Secciones/Seccion Siete/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car();
-        nissan.color = "Gray";
-        nissan.make = "Nissan";
-        nissan.price = 10000;
-        nissan.year = 2009;
-
-        Car dodge = new Car();
-        dodge.year = 2018;
-        dodge.make = "Dodge";
-        dodge.price = 86000;
-        dodge.color = "Orange";
+        Car nissan = new Car("Nissa",10000, 2009, "Gray");
+        Car dodge = new Car("Dodge", 86000, 2018, "Orange");
 
         System.out.println("This " + nissan.make + " is worth $" + nissan.price +
                 ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
