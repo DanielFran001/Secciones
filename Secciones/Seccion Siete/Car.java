df2d2ebf227505aa3f72b643c7545aa1da7ff5d9
Daniel Franco <dfrancohenao@gmail.com>
2023-09-17 16:48:45 -0500

    Copy Constructor

--- Secciones/Seccion Siete/Car.java
+++ Secciones/Seccion Siete/Car.java
@@ -4,26 +4,49 @@ public class Car {
     private int year;
     private String color;
 
-    public Car (String make, double price, int year, String color){
+    public Car(String make, double price, int year, String color) {
         this.make = make;
         this.price = price;
         this.year = year;
         this.color = color;
     }
 
-    public String getMake(){
+    public Car(Car source){
+        this.make = source.make;
+        this.price = source.price;
+        this.year = source.year;
+        this.color = source.color;
+    }
+
+    public String getMake() {
         return make;
     }
 
-    public double getPrice(){
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public double getPrice() {
         return price;
     }
 
-    public int getYear(){
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getYear() {
         return year;
     }
 
-    public String getColor(){
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
         return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
--- Secciones/Seccion Siete/Main.java
+++ Secciones/Seccion Siete/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Car nissan = new Car("Nissan",10000, 2009, "Gray");
+
+        Car nissan2 = new Car(nissan);
+
+        nissan2.setColor("Yellow");
+        nissan.setColor("Orange");
+        nissan2.setColor("Blue");
+        nissan.setColor("Purple");
+        nissan2.setColor("Fuchsia");
+        nissan.setColor("Beige");
+
         Car dodge = new Car("Dodge", 86000, 2018, "Orange");
 
 
