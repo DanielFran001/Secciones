a20bbc433a13a7055970a47e137e5423ad94237d
Daniel Franco <dfrancohenao@gmail.com>
2023-09-17 12:44:37 -0500

    Update

--- Secciones/Seccion Siete/Main.java
+++ Secciones/Seccion Siete/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Car nissan = new Car("Nissa",10000, 2009, "Gray");
+        Car nissan = new Car("Nissan",10000, 2009, "Gray");
         Car dodge = new Car("Dodge", 86000, 2018, "Orange");
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price +
-                ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price +
-                ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() +
+                ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() +
+                ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
     }
 }
