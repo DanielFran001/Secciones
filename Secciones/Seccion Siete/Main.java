0d9c9d9fe19d5046b782b389151610ed02c54f33
Daniel Franco <dfrancohenao@gmail.com>
2023-09-25 18:21:12 -0500

    ToString add

--- Secciones/Seccion Siete/Main.java
+++ Secciones/Seccion Siete/Main.java
@@ -3,14 +3,10 @@ public class Main {
 
         Car nissan = new Car("Nissan",10000, 2009, "Gray", new String[]{"Tires", "Keys"});
         Car dodge = new Car("Dodge", 86000, 2018, "Orange", new String[]{"Tires", "Keys"});
-
         Car nissan2 = new Car(nissan);
-        nissan2.setColor("Orange");
-        nissan2.drive();
-
-        Car nissan3 = new Car(nissan);
-        nissan3.setYear(2024);
-        nissan3.drive();
+        System.out.println(nissan);
+        System.out.println(dodge);
+        System.out.println(nissan2);
 
 
 
